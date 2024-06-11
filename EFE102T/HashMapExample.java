978dc7f96f0dc91cde45669ac364c5121de27cf8
Yahya Efe Kuruçay <109385388+efekurucay@users.noreply.github.com>
2024-06-11 21:07:53 +0300

    update

---
+++ EFE102T/HashMapExample.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        Map<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("Apple", 1);
+        hashMap.put("Banana", 2);
+        hashMap.put("Orange", 3);
+        hashMap.put("Mango", 4);
+
+        for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+    }
+}
